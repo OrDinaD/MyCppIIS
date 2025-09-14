fc383fbe8c66a9b62488010acdd9253cacfb0af9
OrDinaD <Vlad.vasilevskiy.07@gmail.com>
2025-09-14 16:21:22 +0300

    Initial Commit

---
+++ cPPiIS/cPPiISApp.swift
@@ -0,0 +1,17 @@
+//
+//  cPPiISApp.swift
+//  cPPiIS
+//
+//  Created by Влад on 14.09.25.
+//
+
+import SwiftUI
+
+@main
+struct cPPiISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
